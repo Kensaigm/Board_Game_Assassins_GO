d977356af16908ee451bd449347eee48bd0f49f2
Kensaigm <scarroll10@student.cccd.edu>
2017-12-01 17:46:20 -0800

    iTween easetInOut on Y axis, iTween easeBounce on X axis.

--- Assassins GO Board Game/Assets/Scripts/iTweenExample.cs
+++ Assassins GO Board Game/Assets/Scripts/iTweenExample.cs
@@ -6,7 +6,14 @@ public class iTweenExample : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		iTween.RotateTo(gameObject, iTween.Hash("y", 90f));
+		iTween.RotateTo(gameObject, iTween.Hash("y", 90f, "delay", 1f, "time", 2f, "easetype", iTween.EaseType.easeInOutExpo));
+
+		iTween.RotateTo(gameObject, iTween.Hash(
+			"x", 90f,
+			"delay", 2f,
+			"time", 1f,
+			"easetype", iTween.EaseType.easeInBounce
+		));
 	}
 	
 	// Update is called once per frame
