043f0aa9068cebd7704125acdb22b58055b4507d
Kensaigm <scarroll10@student.cccd.edu>
2017-12-03 21:05:48 -0800

    Added iTween ColorTo, MoveTo, and ShakePosition.

--- Assassins GO Board Game/Assets/Scripts/iTweenExample.cs
+++ Assassins GO Board Game/Assets/Scripts/iTweenExample.cs
@@ -9,11 +9,35 @@ public class iTweenExample : MonoBehaviour {
 		iTween.RotateTo(gameObject, iTween.Hash("y", 90f, "delay", 1f, "time", 2f, "easetype", iTween.EaseType.easeInOutExpo));
 
 		iTween.RotateTo(gameObject, iTween.Hash(
-			"x", 90f,
+			"x", -90f,
 			"delay", 2f,
 			"time", 1f,
-			"easetype", iTween.EaseType.easeInBounce
+			"easetype", iTween.EaseType.easeOutBounce
 		));
+
+		iTween.ColorTo(gameObject, iTween.Hash(
+			"r", 0f,
+			"g", 0.5f,
+			"b", 1f,
+			"delay", 5f,
+			"time", 1f,
+			"easetype", iTween.EaseType.easeInOutExpo
+		));
+
+		iTween.MoveTo(gameObject, iTween.Hash(
+			"y", -2f,
+			"delay", 6f,
+			"time", 0.5f,
+			"easetype", iTween.EaseType.easeOutBounce
+		));
+
+		iTween.ShakePosition(Camera.main.gameObject, iTween.Hash(
+			"y", -0.2f,
+			"delay", 6.3f,
+			"time", 0.5f,
+			"easetype", iTween.EaseType.easeOutBounce
+		));
+
 	}
 	
 	// Update is called once per frame
