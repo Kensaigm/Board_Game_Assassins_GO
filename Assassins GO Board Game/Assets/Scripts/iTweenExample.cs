779904910688d4ff5960e60dc51dc175dc67d915
Kensaigm <scott_carroll_99@yahoo.com>
2017-12-10 10:18:56 -0800

    Upgrade to 2017.3.0f2 for MacOS

--- Assassins GO Board Game/Assets/Scripts/iTweenExample.cs
+++ Assassins GO Board Game/Assets/Scripts/iTweenExample.cs
@@ -8,37 +8,45 @@ public class iTweenExample : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		m_MainCamera = Camera.main;
-		iTween.RotateTo(gameObject, iTween.Hash("y", 90f, "delay", 1f, "time", 2f, "easetype", iTween.EaseType.easeInOutExpo));
 
-		iTween.RotateTo(gameObject, iTween.Hash(
-			"x", -90f,
-			"delay", 2f,
-			"time", 1f,
-			"easetype", iTween.EaseType.easeOutBounce
-		));
+        iTween.MoveFrom(gameObject, iTween.Hash(
+          "y", 0f,
+          "delay", 1f,
+            "time", 0.5f,
+            "easetype", iTween.EaseType.easeOutBounce
+        ));
 
-		iTween.ColorTo(gameObject, iTween.Hash(
-			"r", 0f,
-			"g", 0.5f,
-			"b", 1f,
-			"delay", 5f,
-			"time", 1f,
-			"easetype", iTween.EaseType.easeInOutExpo
-		));
+		//iTween.RotateTo(gameObject, iTween.Hash("y", 90f, "delay", 1f, "time", 2f, "easetype", iTween.EaseType.easeInOutExpo));
 
-		iTween.MoveTo(gameObject, iTween.Hash(
-			"y", -2f,
-			"delay", 6f,
-			"time", 0.5f,
-			"easetype", iTween.EaseType.easeOutBounce
-		));
+		//iTween.RotateTo(gameObject, iTween.Hash(
+		//	"x", -90f,
+		//	"delay", 2f,
+		//	"time", 1f,
+		//	"easetype", iTween.EaseType.easeOutBounce
+		//));
 
-		iTween.ShakePosition(m_MainCamera.gameObject, iTween.Hash(
-			"y", -0.2f,
-			"delay", 6.3f,
-			"time", 0.3f,
-			"easetype", iTween.EaseType.easeOutBounce
-		));
+		//iTween.ColorTo(gameObject, iTween.Hash(
+		//	"r", 0f,
+		//	"g", 0.5f,
+		//	"b", 1f,
+		//	"delay", 5f,
+		//	"time", 1f,
+		//	"easetype", iTween.EaseType.easeInOutExpo
+		//));
+
+		//iTween.MoveTo(gameObject, iTween.Hash(
+		//	"y", -2f,
+		//	"delay", 6f,
+		//	"time", 0.5f,
+		//	"easetype", iTween.EaseType.easeOutBounce
+		//));
+
+		//iTween.ShakePosition(m_MainCamera.gameObject, iTween.Hash(
+		//	"y", -0.2f,
+		//	"delay", 6.3f,
+		//	"time", 0.3f,
+		//	"easetype", iTween.EaseType.easeOutBounce
+		//));
 
 	}
 	
