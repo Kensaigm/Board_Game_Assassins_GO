bf92865a47aa02d3a21aa323b88210d560726b08
Kensaigm <scarroll10@student.cccd.edu>
2017-12-13 13:30:25 -0800

    Prep for Floor Scene

--- Assassins GO Board Game/Assets/Scripts/iTweenExample.cs
+++ Assassins GO Board Game/Assets/Scripts/iTweenExample.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class iTweenExample : MonoBehaviour {
+	Camera m_MainCamera;
 
 	// Use this for initialization
 	void Start () {
+		m_MainCamera = Camera.main;
 		iTween.RotateTo(gameObject, iTween.Hash("y", 90f, "delay", 1f, "time", 2f, "easetype", iTween.EaseType.easeInOutExpo));
 
 		iTween.RotateTo(gameObject, iTween.Hash(
@@ -31,10 +33,10 @@ public class iTweenExample : MonoBehaviour {
 			"easetype", iTween.EaseType.easeOutBounce
 		));
 
-		iTween.ShakePosition(Camera.main.gameObject, iTween.Hash(
+		iTween.ShakePosition(m_MainCamera.gameObject, iTween.Hash(
 			"y", -0.2f,
 			"delay", 6.3f,
-			"time", 0.5f,
+			"time", 0.3f,
 			"easetype", iTween.EaseType.easeOutBounce
 		));
 
